dc48deb9971184547eaaded5574f7bfcf36ac6c7
Paolo Ceravolo <paoloceravolo@Paolos-MBP.station>
2020-04-02 13:57:13 +0200

    quarta pagina

--- quarta pagina/clock.js
+++ quarta pagina/clock.js
@@ -1,28 +1,35 @@
-var date = new Date();
+function time(){
 
-let h = date.getHours(); // 0-23
-let m = date.getMinutes(); // 0-59
-let s = date.getSeconds(); // 0-59
-let meridien = "AM";
+	var date = new Date();
 
-console.log(typeof h);
+	let h = date.getHours(); // 0-23
+	let m = date.getMinutes(); // 0-59
+	let s = date.getSeconds(); // 0-59
+	let meridien = "AM";
 
-if(h === 0){h = 12;}
+	console.log(typeof h);
 
-if(h > 12){
-	h = h - 12;
-	meridien = "PM";
+	if(h === 0){h = 12;}
+
+	if(h > 12){
+		h = h - 12;
+		meridien = "PM";
+	}
+
+	h = (h<10)? "0"+h : h;
+	m = (m<10)? "0"+m : m;
+	s = (s<10)? "0"+s : s;
+
+
+	let currTime = h + ":" + m + ":" + s + ":" + meridien;
+	console.log(currTime);
+
+	let xss = "<img src=\"foo.img\" onerror=\"alert(document.cookie)\">";
+
+	document.getElementById("clock").innerText = currTime; 
+	//document.getElementById("clock").innerHTML = xss; 
+	window.setTimeout(time, 1000);
+
 }
 
-h = (h<10)? "0"+h : h;
-m = (m<10)? "0"+m : m;
-s = (s<10)? "0"+s : s;
-
-
-let currTime = h + ":" + m + ":" + s + ":" + meridien;
-console.log(currTime);
-
-let xss = "<img src=\"foo.img\" onerror=\"alert(document.cookie)\">";
-
-document.getElementById("clock").innerText = currTime; 
-//document.getElementById("clock").innerHTML = xss; 
+time();
