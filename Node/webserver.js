a359ebabf5e80072e0021238b59f5982d6d884f6
paoloceravolo <paolo.ceravolo@unimi.it>
2020-05-21 11:37:47 +0200

    Web Server Readdir Errore flusso di callback

--- Node/webserver.js
+++ Node/webserver.js
@@ -3,19 +3,29 @@ const fs = require('fs');
 
 var server = http.createServer();
 
+error = null;
+output = null;
+
+function lista_img(path){
+	fs.readdir(path, (err, files)=>{
+		if(err){error = {'error': err}; return}
+		else{output = {'error': null, 'data': files}; return}
+	});
+}
+
 server.on('request', (req, res)=>{
 	console.log("Incoming request: " +req.method+ " " + req.url);
 	//console.log(req.url);
 	path = process.cwd()+req.url;
 
-	fs.readdir(path, function(err, files){
-		if(err){
+	lista_img(path);
+		if(error !== null){
 			res.writeHead(400, {'Content-Type': 'application/json'});
-			res.end(JSON.stringify(err) + '\n');
+			res.end(JSON.stringify(error) + '\n');
 		}
 		res.writeHead(400, {'Content-Type': 'application/json'});
-		res.end(JSON.stringify(files) + '\n');
-	});
+		res.end(JSON.stringify(output) + '\n');
+
 });
 
 server.on('error', function(err){
